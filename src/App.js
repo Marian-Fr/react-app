ed12949524d707fae726ddda5bd2fb20a9fa37d1
Marian-Fr <126185488+Marian-Fr@users.noreply.github.com>
2023-06-20 16:41:08 +0200

    Added Weather.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Weather from "./Weather"
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <h1>
         Weather React App
       </h1>
+      <Weather />
       <footer>
         This project is created by Maryna Frolova and {" "}
       <a href="https://github.com/Marian-Fr/react-app" rel="noreferrer"  target="_blank">is open-sourced on GitHub</a>
