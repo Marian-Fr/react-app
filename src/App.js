e4f83ed47e416b405c4120b378c7aa3173f5fcf0
Marian-Fr <126185488+Marian-Fr@users.noreply.github.com>
2023-06-20 16:06:57 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Weather React App
+      <h1>
+        Weather React App
+      </h1>
+      <footer>
+        This project is created by Maryna Frolova and {" "}
+      <a href="https://github.com/Marian-Fr/react-app" rel="noreferrer"  target="_blank">is open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
