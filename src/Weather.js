2d95949371084ae5819c5ac35958ce935e401f56
Marian-Fr <126185488+Marian-Fr@users.noreply.github.com>
2023-06-21 18:34:11 +0200

    Added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 import "./Weather.css";
 
 export default function Weather(){
-    return<div className="Weather">
+    const [ready, setReady] = useState(false);
+const [weatherData, setWeatherData] = useState({});
+function handleResponse(response){
+console.log(response.data);
+setWeatherData ({
+temperature: Math.round(response.data.temperature.current),
+wind :response.data.wind,
+humidity:response.data.temperature.current.humidity
+
+    })
+
+
+setReady(true);
+}
+if (ready){
+return(
+    <div className="Weather">
         <form>
             <div className="row">
             <div className="col-9">
@@ -30,22 +48,30 @@ export default function Weather(){
     <div class="row">
     <div class="col-6">
 <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="Cloudy"/>
- <span class="temperature">16</span>
+ <span class="temperature">{weatherData.temperature}</span>
  <span class="celsius">℃</span>
     </div>
     <div class="col-6">
        <ul>
         <li>
-            Precipitation: 31%
+            Humidity:{weatherData.humidity} 88%
         </li>
         <li>
-            Humidity: 88%
-        </li>
-        <li>
-            Wind: 13km/h
+            Wind: {weatherData.wind.speed}km/h
         </li>
        </ul>
     </div>
         </div>
-    </div>;
+    </div>);
 }
+else{
+const apiKey="2e8483398ocdfb8e6befab4eb2at6740";
+    let city = "New York";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading..."
+}
+    
+
+} 
